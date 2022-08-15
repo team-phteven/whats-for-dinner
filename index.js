cae1558008d369846923627e610d3c9d83f46ea7
rikifujihara <rikifujihara@gmail.com>
2022-08-15 17:10:27 +1000

    tweaked camelCase function

--- index.js
+++ index.js
@@ -50,12 +50,12 @@ const diets = [
 
 // function to convert string to camelCase
 function toCamelCase(str) {
-    if (str === "") return ""
     let answer = ""
-    for (let i =0; i < str.length; i++){
+    answer += str[0].toLowerCase()
+    for (let i =1; i < str.length; i++){
         if (str[i - 1] === "_"||str[i - 1] === "-"){
             answer += str[i].toUpperCase()
-        } else if (str[i] !== "-" && str[i] !== "_"){
+        } else if (str[i] !== "-" && str[i] !== "_" && str[i] !== " "){
             answer += str[i]
         }
     }
@@ -66,7 +66,7 @@ function toCamelCase(str) {
 function populateSelectOptions(element, options) {
     for (let option of options) {
         const newOption = document.createElement('option');
-        newOption.value = option;
+        newOption.value = toCamelCase(option);
         newOption.text = option;
         element.appendChild(newOption);
     }
@@ -87,16 +87,22 @@ const SPOONAPI = "https://api.spoonacular.com/recipes"
 let cuisine = "mediterranean"
 let diet = "vegetarian"
 
-async function getRecipeId() {
+async function getRecipeInfo() {
+
+    return {
+        "cuisine": "cuisineSelect.value",
+        "diet": "dietSelect.value"
+    }
+
     // let res = await fetch(`${SPOONAPI}/complexSearch?apiKey=${API_KEY}&cuisine=${cuisineSelect.value}&diet=${dietSelect.value}&number=1`)
-    let res = await fetch(`${SPOONAPI}/complexSearch?apiKey=${API_KEY}&cuisine=${cuisineSelect.value}&diet=${dietSelect.value}&number=1`)
-    let body = await res.json()
-    let result = await body.results[0]
-    let id = await result.id
-    console.log(id)
-    return id
+    // let body = await res.json()
+    // let result = await body.results[0]
+    // console.log(result)
+    // let id = await result.id
+    // console.log(id)
+    // return id
     // id title image
-    //
+    
 }
 
 async function getRecipeCard() {
@@ -107,3 +113,22 @@ async function getRecipeCard() {
     // let result = await body.results[0]
     console.log(body)
 }
+
+// console.log(toCamelCase("Lacto-Vegetarian"))
+
+// click event listener for button
+
+// const recipeInfo = {}
+
+document.querySelector('button').addEventListener('click', function(event) {
+    event.preventDefault()
+    console.log(getRecipeInfo())
+})
+
+// sample response from getRecipeInfo
+
+// {id: 716268, title: 'African Chicken Peanut Stew', image: 'https://spoonacular.com/recipeImages/716268-312x231.jpg', imageType: 'jpg'}
+// id: 716268
+// image: "https://spoonacular.com/recipeImages/716268-312x231.jpg"
+// imageType: "jpg"
+// title: "African Chicken Peanut Stew"
